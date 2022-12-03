b1be0e4c6fd29d8b731aa7023b8c6cf305a8b821
Peter Huss <pbhuss@users.noreply.github.com>
2022-12-03 15:40:32 -0800

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+import pkgutil
+import re
+from importlib import import_module
+
+import pytest
+
+import solutions
+
+
+def load_solution_modules():
+    module_map = {}
+    pattern = re.compile(r"(\d{4})\.(\d{2})")
+    for _, modname, is_pkg in pkgutil.walk_packages(
+        solutions.__path__, f"{solutions.__name__}."
+    ):
+        if is_pkg:
+            continue
+        match = pattern.search(modname)
+        if match is None:
+            raise Exception(f"invalid module {modname}")
+        problem = f"{match.group(1)}-{match.group(2)}"
+        module_map[problem] = import_module(modname)
+    return module_map
+
+
+def pytest_addoption(parser):
+    solution_modules = load_solution_modules()
+    parser.addoption(
+        "--solutions",
+        action="store",
+        nargs="*",
+        choices=solution_modules,
+        default=solution_modules,
+        help="solution numbers to test (default: all)",
+    )
+
+
+def pytest_generate_tests(metafunc):
+    metafunc.parametrize("problem", metafunc.config.option.solutions)
+
+
+@pytest.fixture(scope="session")
+def solution_modules():
+    return load_solution_modules()
