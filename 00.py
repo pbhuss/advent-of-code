c49f075b143e6a7945549ccc351f38c731e7b30c
Peter Huss <pbhuss@users.noreply.github.com>
2022-12-07 11:40:25 -0800

    Add template

---
+++ 00.py
@@ -0,0 +1,13 @@
+from libaoc import SolutionBase
+
+
+class Solution(SolutionBase):
+    def part1(self):
+        return
+
+    def part2(self):
+        return
+
+
+if __name__ == "__main__":
+    Solution().run()
