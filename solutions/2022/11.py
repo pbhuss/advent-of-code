287478fbc298ca1e9b71735b610bc33d57391856
Peter Huss <pbhuss@users.noreply.github.com>
2022-12-11 21:37:10 -0800

    Enhancements for 2022-11

--- solutions/2022/11.py
+++ solutions/2022/11.py
@@ -1,8 +1,10 @@
 import logging
+import math
 import operator
 import re
 from collections.abc import Callable
-from dataclasses import dataclass
+from functools import partial
+from typing import NamedTuple
 
 from more_itertools import chunked
 
@@ -13,27 +15,17 @@ logging.basicConfig(level=logging.ERROR, format="%(message)s")
 logger = logging.getLogger(__name__)
 
 
-@dataclass
-class Monkey:
+class Monkey(NamedTuple):
 
     items: list[int]
-    op: Callable[[int, int], int]
-    op_right: str
+    operation: Callable[[int], int]
     divisor: int
     true_idx: int
     false_idx: int
-    inspect_count: int = 0
 
-    def update(self, item: int, divide: bool, modulo: int = 0) -> int:
-        if self.op_right == "old":
-            val = self.op(item, item)
-        else:
-            val = self.op(item, int(self.op_right))
-        if divide:
-            val //= 3
-        else:
-            val %= modulo
-        return val
+
+def square(x: int) -> int:
+    return x**2
 
 
 class Solution(SolutionBase):
@@ -45,34 +37,39 @@ class Solution(SolutionBase):
             items = list(map(int, num_pattern.findall(lines[1])))
             op_str, op_right = operation_re.search(lines[2]).groups((2, 3))
             op = operator.add if op_str == "+" else operator.mul
-
+            if op_right == "old":
+                operation = square
+            else:
+                operation = partial(op, int(op_right))
             divisor, true_idx, false_idx = map(
                 int, (num_pattern.search(line).group(0) for line in lines[3:6])
             )
             monkeys.append(
                 Monkey(
                     items=items,
-                    op=op,
-                    op_right=op_right,
+                    operation=operation,
                     divisor=divisor,
                     true_idx=true_idx,
                     false_idx=false_idx,
                 )
             )
 
-        modulo = 1
-        for monkey in monkeys:
-            modulo *= monkey.divisor
+        modulo = math.prod(monkey.divisor for monkey in monkeys)
+        inspect_counts = [0 for _ in monkeys]
 
-        for round in range(num_rounds):
+        for _ in range(num_rounds):
             for i, monkey in enumerate(monkeys):
                 logging.info(f"Monkey {i}")
-                monkey.inspect_count += len(monkey.items)
+                inspect_counts[i] += len(monkey.items)
                 for item in monkey.items:
                     logging.info(
                         f"  Monkey inspects an item with a worry value of {item}."
                     )
-                    item = monkey.update(item, divide, modulo)
+                    item = monkey.operation(item)
+                    if divide:
+                        item //= 3
+                    else:
+                        item %= modulo
                     logging.info(f"    Worry level is modified to {item}.")
                     if item % monkey.divisor == 0:
                         logging.info(
@@ -90,13 +87,13 @@ class Solution(SolutionBase):
                             f"    Item with worry level {item} is thrown to monkey {monkey.false_idx}."
                         )
                         monkeys[monkey.false_idx].items.append(item)
-                monkey.items = []
+                monkey.items.clear()
 
-        for i, monkey in enumerate(monkeys):
-            logging.info(f"Monkey {i} inspected items {monkey.inspect_count} times.")
+        for i, inspect_count in enumerate(inspect_counts):
+            logging.info(f"Monkey {i} inspected items {inspect_count} times.")
 
-        top_counts = sorted(monkey.inspect_count for monkey in monkeys)[-2:]
-        return top_counts[0] * top_counts[1]
+        inspect_counts.sort()
+        return inspect_counts[-1] * inspect_counts[-2]
 
     def part1(self):
         return self.solve(num_rounds=20, divide=True)
