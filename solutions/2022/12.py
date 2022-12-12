0d2d2c2a9a74d66da55a74765c0c475aed204662
Peter Huss <pbhuss@users.noreply.github.com>
2022-12-11 23:27:30 -0800

    Add solution for 2022-12

---
+++ solutions/2022/12.py
@@ -0,0 +1,113 @@
+import math
+from queue import PriorityQueue
+
+from libaoc import SolutionBase
+
+
+def neighbors(grid, x, y):
+    return [
+        (x + i, y + j)
+        for i, j in [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        if (x + i, y + j) in grid and grid[(x + i, y + j)] - grid[(x, y)] <= 1
+    ]
+
+
+def neighbors_reverse(grid, x, y):
+    return [
+        (x + i, y + j)
+        for i, j in [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        if (x + i, y + j) in grid and grid[(x, y)] - grid[(x + i, y + j)] <= 1
+    ]
+
+
+# TODO: DRY this up
+class Solution(SolutionBase):
+    def part1(self):
+        grid = {}
+        start = None
+        end = None
+        for y, line in enumerate(self.input()):
+            for x, val in enumerate(line):
+                match val:
+                    case "S":
+                        start = (x, y)
+                        val = "a"
+                    case "E":
+                        end = (x, y)
+                        val = "z"
+                grid[(x, y)] = ord(val) - ord("a")
+
+        n_col, n_row = map(lambda x: x + 1, max(grid))
+
+        dists = {}
+        visited = set()
+        dists[start] = 0
+
+        queue = PriorityQueue()
+        queue.put((0, start))
+        for x in range(n_col):
+            for y in range(n_row):
+                pos = (x, y)
+                if pos != start:
+                    dists[pos] = math.inf
+
+        while not queue.empty():
+            dist, pos = queue.get()
+            if pos == end:
+                return dists[end]
+            if pos in visited:
+                continue
+            visited.add(pos)
+            for neighbor in neighbors(grid, *pos):
+                alt_dist = dist + 1
+                if alt_dist < dists[neighbor]:
+                    dists[neighbor] = alt_dist
+                    queue.put((alt_dist, neighbor))
+
+    def part2(self):
+        grid = {}
+        start = None
+        ends = []
+        for y, line in enumerate(self.input()):
+            for x, val in enumerate(line):
+                match val:
+                    case "S":
+                        ends.append((x, y))
+                        val = "a"
+                    case "E":
+                        start = (x, y)
+                        val = "z"
+                    case "a":
+                        ends.append((x, y))
+                grid[(x, y)] = ord(val) - ord("a")
+
+        n_col, n_row = map(lambda x: x + 1, max(grid))
+
+        dists = {}
+        visited = set()
+        dists[start] = 0
+
+        queue = PriorityQueue()
+        queue.put((0, start))
+        for x in range(n_col):
+            for y in range(n_row):
+                pos = (x, y)
+                if pos != start:
+                    dists[pos] = math.inf
+
+        while not queue.empty():
+            dist, pos = queue.get()
+            if pos in visited:
+                continue
+            visited.add(pos)
+            for neighbor in neighbors_reverse(grid, *pos):
+                alt_dist = dist + 1
+                if alt_dist < dists[neighbor]:
+                    dists[neighbor] = alt_dist
+                    queue.put((alt_dist, neighbor))
+
+        return min(dists[end] for end in ends)
+
+
+if __name__ == "__main__":
+    Solution().run()
