cd92c54918f5a8caa3b0764a3888d89d933753e7
Peter Huss <pbhuss@users.noreply.github.com>
2022-12-16 00:41:15 -0800

    Add solution for 2022-15

---
+++ solutions/2022/15.py
@@ -0,0 +1,59 @@
+import re
+
+from tqdm import trange
+
+from libaoc import SolutionBase
+
+
+class Solution(SolutionBase):
+    def part1(self, y_check=2000000):
+        pattern = re.compile(
+            r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
+        )
+        x_no_beacon = set()
+        x_beacon = set()
+
+        for line in self.input():
+            sensor_x, sensor_y, beacon_x, beacon_y = map(
+                int, pattern.match(line).group(1, 2, 3, 4)
+            )
+            if beacon_y == y_check:
+                x_beacon.add(beacon_x)
+            beacon_dist = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
+            diff = beacon_dist - abs(sensor_y - y_check)
+            for xi in range(sensor_x - diff, sensor_x + diff + 1):
+                x_no_beacon.add(xi)
+
+        return len(x_no_beacon - x_beacon)
+
+    def part2(self, max_pos=4000000):
+        pattern = re.compile(
+            r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
+        )
+        min_pos = 0
+        sensors = []
+        for line in self.input():
+            sensor_x, sensor_y, beacon_x, beacon_y = map(
+                int, pattern.match(line).group(1, 2, 3, 4)
+            )
+            beacon_dist = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
+            sensors.append(((sensor_x, sensor_y), beacon_dist))
+
+        for xi in trange(min_pos, max_pos + 1):
+            yi = min_pos
+            while yi <= max_pos:
+                covered = False
+                for (sensor_x, sensor_y), beacon_dist in sensors:
+                    dist = abs(xi - sensor_x) + abs(yi - sensor_y)
+                    if dist <= beacon_dist:
+                        covered = True
+                        yi += beacon_dist - dist
+                        break
+                if not covered:
+                    return xi * max_pos + yi
+                yi += 1
+        raise Exception("unreachable")
+
+
+if __name__ == "__main__":
+    Solution().run()
