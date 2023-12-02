a3f1eb5dc9b46ba28f8d1300d57334dbc639676e
Peter Huss <pbhuss@users.noreply.github.com>
2023-12-02 13:21:15 -0800

    Add solutions for 2023-01 and 2023-02

---
+++ solutions/2023/01.py
@@ -0,0 +1,44 @@
+import re
+
+from libaoc import SolutionBase
+
+
+DIGITS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+class Solution(SolutionBase):
+    def part1(self):
+        pattern = re.compile(r"\d")
+        total = 0
+        for line in self.input():
+            matches = pattern.findall(line)
+            total += int(matches[0] + matches[-1])
+        return total
+
+    def part2(self):
+        pattern = re.compile(
+            rf'(?=(\d|{"|".join(DIGITS)}))'
+        )  # positive lookahead for match overlaps
+        total = 0
+        for line in self.input():
+            matches = pattern.findall(line)
+            first = DIGITS.get(matches[0], matches[0])
+            last = DIGITS.get(matches[-1], matches[-1])
+            num = int(first + last)
+            assert 11 <= num <= 99
+            total += num
+        return total
+
+
+if __name__ == "__main__":
+    Solution().run()
---
+++ solutions/2023/02.py
@@ -0,0 +1,48 @@
+import re
+from dataclasses import dataclass
+from dataclasses import field
+
+from libaoc import SolutionBase
+
+
+@dataclass
+class Color:
+    name: str
+    threshold: int
+    pattern: re.Pattern = field(init=False)
+
+    def __post_init__(self):
+        self.pattern = re.compile(rf"(\d+) {self.name}")
+
+
+COLORS = [Color("red", 12), Color("green", 13), Color("blue", 14)]
+
+
+class Solution(SolutionBase):
+    def part1(self):
+        total = 0
+        for idx, line in enumerate(self.input(), start=1):
+            if all(
+                all(
+                    int(count) <= color.threshold
+                    for count in color.pattern.findall(line)
+                )
+                for color in COLORS
+            ):
+                total += idx
+
+        return total
+
+    def part2(self):
+        total = 0
+        for idx, line in enumerate(self.input(), start=1):
+            power = 1
+            for color in COLORS:
+                power *= max(map(int, color.pattern.findall(line)))
+            total += power
+
+        return total
+
+
+if __name__ == "__main__":
+    Solution().run()
